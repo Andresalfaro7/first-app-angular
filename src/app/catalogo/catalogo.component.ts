66667dcf7e69704cc799b2f090c1ec437b2e663a
Andresalfaro7 <andresalfaroparada@gmail.com>
2024-08-19 18:49:51 -0600

    Upload project

---
+++ src/app/catalogo/catalogo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalogo.component.html',
+  styleUrl: './catalogo.component.css'
+})
+export class CatalogoComponent {
+  classmate1:string = "Christian Armando Flores";
+  classmate2:string = "Oscar Daniel Trujillo";
+  classmate3:string = "Enot Carreño Hernandez";
+}
